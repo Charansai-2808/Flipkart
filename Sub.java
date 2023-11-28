62c1e799081e5283b1a631b0a040301976255ad3
Charan <charansai2808@gmail.com>
2023-11-28 12:41:38 +0530

    Data added

---
+++ Sub.java
@@ -0,0 +1,16 @@
+public class JavaProgram
+{
+    public static void main(String args[])
+  {
+    int first, second, add, subtract, multiply;
+    float devide;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter Two Numbers : ");
+    first = scanner.nextInt();
+    second = scanner.nextInt();
+    subtract = first - second;
+    System.out.println("Difference = " + subtract);
+ }
+}
+
